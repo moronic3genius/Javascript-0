d82ff0edfc107b9b9a6e771ef8768eecd1f38184
Melodic Greed <cgayle164@gmail.com>
2022-03-26 17:18:46 -0400

    Commit for question 1

--- Solve/arrays.js
+++ Solve/arrays.js
@@ -76,6 +76,15 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
 console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*`);
 
+for(let x = 0; x < inventory.length; x++) {
+    if (inventory[x].id === 33){
+        console.log(`Car 33 is a ${inventory[32].car_year} ${inventory[32].car_make} ${inventory[32].car_model}`
+        )
+    }
+}
+//Iterating through the inventory of the cars
+//Write a conditional statement that checks if the ID is exactly 33
+//Console log the value
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = 0;
